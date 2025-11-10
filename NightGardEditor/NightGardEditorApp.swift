6991c40c4943124702c4a4dbc8bbee82dc41b750
Michael Fluharty <michael.fluharty@mac.com>
2025-11-09 18:18:26 -0600

    Initial Commit

---
+++ NightGardEditor/NightGardEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightGardEditorApp.swift
+//  NightGardEditor
+//
+//  Created by Michael Fluharty on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct NightGardEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NightGardEditorTests/NightGardEditorTests.swift
@@ -0,0 +1,16 @@
+//
+//  NightGardEditorTests.swift
+//  NightGardEditorTests
+//
+//  Created by Michael Fluharty on 11/9/25.
+//
+
+import Testing
+
+struct NightGardEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
